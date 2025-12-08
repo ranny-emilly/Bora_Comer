04e66a86a784455c656b3e64937fa7e532fb7a20
Emilly Ranny <ranny.emilly@discente.ufg.br>
2025-12-08 15:31:34 -0300

    Create pedidos.c with menu and function stubs
    Initial implementation of the pedidos module with menu and function stubs.

---
+++ src/pedidos.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pedidos.h"
+
+void menuPedidos() {
+    printf("\n--- Módulo de Pedidos/Cozinha (Em Desenvolvimento) ---\n");
+    // TODO (Elisa): Implementar o submenu da cozinha aqui
+}
+
+void adicionarPedido(Pedido** topo) {
+    // TODO (Elisa): Implementar PUSH
+}
+
+void entregarPedido(Pedido** topo) {
+    // TODO (Elisa): Implementar POP
+}
+
+void listarPedidos(Pedido* topo) {
+    // TODO (Elisa): Implementar listagem
+}
