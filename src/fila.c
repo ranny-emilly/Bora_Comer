45d2295d42d5b7defac31da3d7bf2cd2e80e18c7
Emilly Ranny <ranny.emilly@discente.ufg.br>
2025-12-08 15:30:40 -0300

    Create fila.c

---
+++ src/fila.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fila.h"
+
+void menuFila() {
+    printf("\n--- Módulo de Fila de Espera (Em Desenvolvimento) ---\n");
+    // TODO (Emilly): Implementar o submenu da fila aqui
+}
+
+void entrarNaFila(Cliente** inicio, Cliente** fim) {
+    // TODO (Emilly): Implementar Enqueue
+}
+
+void chamarCliente(Cliente** inicio, Cliente** fim) {
+    // TODO (Emilly): Implementar Dequeue
+}
+
+void mostrarFila(Cliente* inicio) {
+    // TODO (Emilly): Implementar listagem
+}
