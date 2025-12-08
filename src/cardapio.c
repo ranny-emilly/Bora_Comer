635e5b370a7345cbdd93bb6dbcfd5cf21ccab273
Emilly Ranny <ranny.emilly@discente.ufg.br>
2025-12-08 15:29:41 -0300

    Add initial cardapio.c with menu functions

---
+++ src/cardapio.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cardapio.h"
+
+void menuCardapio() {
+    printf("\n--- Módulo de Cardápio (Em Desenvolvimento) ---\n");
+    // TODO (Jolie): Implementar o submenu do cardápio aqui
+}
+
+void adicionarItem() {
+    // TODO (Jolie): Implementar adição
+}
+
+void verCardapio() {
+    // TODO (Jolie): Implementar visualização
+}
