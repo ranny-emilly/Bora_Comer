a8fc2a267ed6e76e190c73b30a015d82c2946656
Emilly Ranny <ranny.emilly@discente.ufg.br>
2025-12-08 15:28:27 -0300

    Create funcionarios.c with employee module functions
    Added initial implementation for employee module with menu and placeholder functions.

---
+++ src/funcionarios.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funcionarios.h"
+
+void menuFuncionarios() {
+    printf("\n--- Módulo de Funcionários (Em Desenvolvimento) ---\n");
+    // TODO (Gabs): Implementar o submenu de funcionários aqui
+    // Ex: 1. Cadastrar, 2. Listar, 0. Voltar
+}
+
+void cadastrarFuncionario() {
+    // TODO (Gabs): Implementar cadastro
+}
+
+void listarFuncionarios() {
+    // TODO (Gabs): Implementar listagem
+}
