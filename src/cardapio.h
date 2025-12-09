e74983c23e8cd97b13c4959dcbc9e67478721f94
Jolie Pavan <jolie@discente.ufg.br>
2025-12-09 19:51:21 -0300

    Update cardapio.h

--- src/cardapio.h
+++ src/cardapio.h
@@ -1,16 +1,22 @@
 #ifndef CARDAPIO_H
 #define CARDAPIO_H
 
-typedef struct {
-    int id;
-    char nome[50];
-    float preco;
-    char categoria[30]; // Ex: Bebida, Prato Principal
+
+// Estrutura para itens do cardápio (lista)
+typedef struct ItemCardapio {
+int id;
+char nome[50];
+float preco;
+char categoria[30];
+struct ItemCardapio* proximo;
 } ItemCardapio;
 
 // Protótipos
 void menuCardapio();
 void adicionarItem();
+void removerItem();
 void verCardapio();
 
 #endif
+
+
