2bc6a27d3ea81941192fe9d2ceebf48ef6006db8
gabriela brasileiro <gabrielabrasileiro@discente.ufg.br>
2025-12-10 08:10:12 -0300

    Remove comments from funcionarios.h

--- src/funcionarios.h
+++ src/funcionarios.h
@@ -1,14 +1,12 @@
 #ifndef FUNCIONARIOS_H
 #define FUNCIONARIOS_H
 
-// Estrutura para um funcionário
 typedef struct {
     int id;
     char nome[50];
     char cargo[30];
 } Funcionario;
 
-// Protótipos das funções (O "Menu" do que este módulo faz)
 void menuFuncionarios();
 void cadastrarFuncionario();
 void listarFuncionarios();
