b4683306092a34cf3b9a0dc8da2fd8adc7f06985
Emilly Ranny <ranny.emilly@discente.ufg.br>
2025-12-10 16:35:31 -0300

    Update fila.h

--- src/fila.h
+++ src/fila.h
@@ -1,18 +1,17 @@
 #ifndef FILA_H
 #define FILA_H
 
-// Estrutura de Nó para a Fila (Queue) - Implementa uma Lista Encadeada
-// Requisito 3: Fila de espera de clientes
+
 typedef struct Cliente {
     char nome[50];
     char telefone[15];
-    struct Cliente* proximo; // Ponteiro para o próximo cliente na fila
+    struct Cliente* proximo; 
 } Cliente;
 
-// Protótipos das funções que serão usadas pelo menu e outros módulos
+
 void menuFila();
-void entrarNaFila(Cliente** inicio, Cliente** fim); // Função para adicionar (Enqueue)
-void chamarCliente(Cliente** inicio, Cliente** fim); // Função para remover (Dequeue)
-void mostrarFila(Cliente* inicio); // Função para listar
+void entrarNaFila(Cliente** inicio, Cliente** fim); 
+void chamarCliente(Cliente** inicio, Cliente** fim); 
+void mostrarFila(Cliente* inicio); 
 
 #endif
